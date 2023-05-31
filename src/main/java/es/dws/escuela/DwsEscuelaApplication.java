ad0c61c4b996995310382e03762b3bfe47328eb7
Carel Sánchez <carel3d@gmail.com>
2023-05-31 09:52:41 +0200

    Unversioned files

---
+++ src/main/java/es/dws/escuela/DwsEscuelaApplication.java
@@ -0,0 +1,13 @@
+package es.dws.escuela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwsEscuelaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwsEscuelaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/dws/escuela/DwsEscuelaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.dws.escuela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwsEscuelaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
